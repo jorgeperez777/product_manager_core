89ee5018409a65e3610f6e1a420fa12e8e3cffe9
jorgeperez777 <jorgearmandoperezmedina@gmail.com>
2026-03-04 15:50:02 -0500

    se agregan autenticacion de usuario con login

---
+++ lib/product_manager_core_web/live/home_live/index.ex
@@ -0,0 +1,18 @@
+defmodule ProductManagerCoreWeb.HomeLive.Index do
+  use ProductManagerCoreWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, index_params: nil) |> assign(:current_page, :dashboard_home)}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(%{assigns: _assigns} = socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Dasboard Principal")
+  end
+end
--- lib/product_manager_core_web/router.ex
+++ lib/product_manager_core_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ProductManagerCoreWeb.Router do
   use ProductManagerCoreWeb, :router
 
+  import ProductManagerCoreWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,17 +10,18 @@ defmodule ProductManagerCoreWeb.Router do
     plug :put_root_layout, html: {ProductManagerCoreWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", ProductManagerCoreWeb do
-    pipe_through :browser
+  # scope "/", ProductManagerCoreWeb do
+  #   pipe_through :browser
 
-    get "/", PageController, :home
-  end
+  #   get "/", PageController, :home
+  # end
 
   # Other scopes may use custom stacks.
   # scope "/api", ProductManagerCoreWeb do
@@ -39,6 +42,46 @@ defmodule ProductManagerCoreWeb.Router do
 
       live_dashboard "/dashboard", metrics: ProductManagerCoreWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ProductManagerCoreWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ProductManagerCoreWeb.UserAuth, :redirect_if_user_is_authenticated}],
+      layout: {ProductManagerCoreWeb.Layouts, :login} do
+      live "/register", UserRegistrationLive, :new
+      live "/", UserLoginLive, :new
+      live "/reset_password", UserForgotPasswordLive, :new
+      live "/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/login", UserSessionController, :create
+  end
+
+  scope "/", ProductManagerCoreWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ProductManagerCoreWeb.UserAuth, :ensure_authenticated}] do
+      live "/dashboard", HomeLive.Index, :index
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ProductManagerCoreWeb do
+    pipe_through [:browser]
+
+    delete "/logout", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ProductManagerCoreWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
